291f22888e1cd126ff8b4a77c6ce969abd15ab99
Denzel Renner <rennerdenz@gmail.com>
2026-02-04 10:36:24 +0100

    added mapping of reads to hap1, with hisat2

--- process_rnaseq.sh
+++ process_rnaseq.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J process_rna_seq
+#SBATCH -J process_rna_seq_v2
 #SBATCH -A naiss2025-5-531
 #SBATCH -p shared
-#SBATCH --mem=80GB
-#SBATCH -t 2:00:00
+#SBATCH --mem=200GB
+#SBATCH -t 24:00:00
 #SBATCH -o %x.out
 #SBATCH -e %x.err
 
@@ -17,6 +17,7 @@ module load PDCOLD/23.12
 module load openjdk/11.0.20.1_1-gcc-aua
 
 # define inputs
+hap1=~/Anastatica_hierochuntica/Haplome1/Assembly/haplome1.fa
 outputdir=~/Anastatica_hierochuntica/RNAseq/PRJNA731383
 rnaseqdir=~/Anastatica_hierochuntica/RNAseq/PRJNA731383/runs
 project=PRJNA731383
@@ -33,7 +34,7 @@ mkdir -p multiqc
 
 conda activate fastqc
 
-fastqc -t 20 -o fastqc -f fastq $rnaseqdir/*
+# fastqc -t 20 -o fastqc -f fastq $rnaseqdir/*
 
 conda deactivate
 
@@ -41,7 +42,65 @@ conda deactivate
 
 conda activate multiqc
 
-multiqc --filename $project --outdir multiqc fastqc
+# multiqc --filename $project --outdir multiqc fastqc
+
+conda deactivate
+
+cd ../
+mkdir -p s2_trimming_reads
+cd s2_trimming_reads
+mkdir -p trimmed_fastqs
+mkdir -p fastqc
+mkdir -p multiqc
+
+# trim reads
+conda activate trim-galore
+
+# trim_galore --cores 20 -o ./trimmed_fastqs $rnaseqdir/*
+
+conda deactivate
+
+# rerun fastqc and multiqc
+# run fastqc
+
+conda activate fastqc
+
+# fastqc -t 20 -o fastqc -f fastq trimmed_fastqs/*.fq
+
+conda deactivate
+
+# run multiqc
+
+conda activate multiqc
+
+# multiqc --filename $project --outdir multiqc fastqc
+
+conda deactivate
+
+cd ../
+mkdir -p s3_alignment
+cd s3_alignment
+
+conda activate hisat2
+
+# create index
+
+# hisat2-build -f -p 10 $hap1 index
+
+# use hisat2 to map reads to genome
+#../s2_trimming_reads/trimmed_fastqs/*.fq
+hisat2 -p 32 -t --dta -x index -U $(echo ../s2_trimming_reads/trimmed_fastqs/*.fq | tr ' ' ',') -S all_mapped.sam
+
+conda deactivate
+
+# bam conversion sorting and indexing
+conda activate minimap2_samtools
+
+samtools view -bS --threads 32 all_mapped.sam > all_mapped.bam
+
+samtools sort --threads 32 all_mapped.bam -o all_mapped.sorted.bam
+
+samtools index -@ 32 all_mapped.sorted.bam
 
 conda deactivate
 
